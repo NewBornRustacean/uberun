ff28786caba058407892dd85c66214a93f68c026
NewBornRustacean <huiseomkim@gmail.com>
2023-04-17 21:03:04 +0900

    텔레그램 봇 추가, commands 구현, artifacts.rs 생성

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,68 @@
 use std::collections::HashMap;
 use std::fs::File;
 
-use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
-
+use teloxide::{prelude::*, utils::command::BotCommands};
 use urlencoding::encode;
 extern crate tokio;
 
+mod artifacts;
+use artifacts::{ClientConfig, ClientResponse};
+
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-
-#[derive(Serialize, Deserialize)]
-struct ClientConfig {
-    seoul_url: String,
-    file_type: String,
-    service_name: String,
-    start_index: String,
-    end_index: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct RealTimeArrival {
-    subwayId: Option<String>,
-    updnLine: Option<String>,
-    trainLineNm: Option<String>,
-    statnFid: Option<String>,
-    statnTid: Option<String>,
-    statnId: Option<String>,
-    statnNm: Option<String>,
-    trnsitCo: Option<String>,
-    ordkey: Option<String>,
-    subwayList: Option<String>,
-    statnList: Option<String>,
-    btrainSttus: Option<String>,
-    barvlDt: Option<String>,
-    btrainNo: Option<String>,
-    bstatnId: Option<String>,
-    bstatnNm: Option<String>,
-    recptnDt: Option<String>,
-    arvlMsg2: Option<String>,
-    arvlMsg3: Option<String>,
-    arvlCd: Option<String>,
-}
-
-#[derive(Deserialize, Debug)]
-struct ServerMessage {
-    status: u8,
-    code: Option<String>,
-    message: Option<String>,
-    link: Option<String>,
-    developerMessage: Option<String>,
-    total: u8,
-}
-
-#[derive(Deserialize, Debug)]
-struct ClientResponse {
-    errorMessage: ServerMessage,
-    realtimeArrivalList: Vec<RealTimeArrival>,
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // read api-key and make request url
     let api_key = get_public_api_key("src/public_subway_api_key.yml");
-    let client_config: ClientConfig = get_client_config("src/client_config.yaml");
+    let client_config: artifacts::ClientConfig = get_client_config("src/client_config.yaml");
 
     let url = make_url(api_key, client_config, "동천".to_string());
     let response = reqwest::get(url).await?.json::<ClientResponse>().await?;
 
     println!("{:?}", response);
+
+    pretty_env_logger::init();
+    log::info!("Starting command bot...");
+
+    let bot = Bot::from_env();
+
+    Command::repl(bot, answer_from_bot).await;
     return Ok(());
+}
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+pub enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "start to watch")]
+    Go(String),
+    #[command(description = "stop to watch ")]
+    Stop,
+}
+
+async fn answer_from_bot(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+
+        Command::Go(station) => {
+            bot.send_message(msg.chat.id, format!("watching {station}.."))
+                .await?
+        }
+        Command::Stop => {
+            bot.send_message(msg.chat.id, format!("stop to watching."))
+                .await?
+        }
+    };
+
+    Ok(())
 }
 
 fn get_public_api_key(api_key_path: &str) -> String {
